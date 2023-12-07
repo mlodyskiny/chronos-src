d062deeb5775d0f233e9e746797cf670d23196fa
parkie <69488850+mlodyskiny@users.noreply.github.com>
2023-12-07 20:48:37 +0100

    leaked

---
+++ evo.gj/cheat/sdk/sdk.cpp
@@ -0,0 +1,64 @@
+#include "sdk.h"
+
+Vector3 SDK::GetBoneWithRotation(uintptr_t mesh, int id)
+{
+	int IsCached = read<int>(mesh + 0x648);
+	auto BoneTransform = read<FTransform>(read<uintptr_t>(mesh + 0x10 * IsCached + 0x600) + 0x60 * id);
+	
+	FTransform ComponentToWorld = read<FTransform>(mesh + 0x240);
+
+	D3DMATRIX Matrix;
+	Matrix = MatrixMultiplication(BoneTransform.ToMatrixWithScale(), ComponentToWorld.ToMatrixWithScale());
+
+	return Vector3(Matrix._41, Matrix._42, Matrix._43);
+}
+struct CamewaDescwipsion
+{
+	Vector3 Location;
+	Vector3 Rotation;
+	float FieldOfView;
+	char Useless[0x18];
+};
+Camera SDK::GetViewAngles() {
+	char v1; // r8
+	CamewaDescwipsion ViewPoint = read<CamewaDescwipsion>(BaseId + 0x10CB8D0);
+	BYTE* v2 = (BYTE*)&ViewPoint;
+	int i; // edx
+	__int64 result; // rax
+
+	v1 = 0x40;
+	for (i = 0; i < 0x40; ++i)
+	{
+		*v2 ^= v1;
+		result = (unsigned int)(i + 0x17);
+		v1 += i + 0x17;
+		v2++;
+	}
+
+	return { ViewPoint.Location, ViewPoint.Rotation, ViewPoint.FieldOfView };
+}
+
+Vector2 SDK::ProjectWorldToScreen(Vector3 WorldLocation)
+{
+
+	vCamera = SDK::GetViewAngles(); //get ur players newest view angles
+	if (Debug::PrintLocations) {
+		Util::Print3D("Location: ", vCamera.Location);
+		Util::Print3D("Rotation: ", vCamera.Rotation);
+		Util::Print2D("Fov: ", Vector2(vCamera.FieldOfView, 69));
+	}
+
+	D3DMATRIX tempMatrix = Matrix(vCamera.Rotation);
+
+	Vector3 vAxisX = Vector3(tempMatrix.m[0][0], tempMatrix.m[0][1], tempMatrix.m[0][2]);
+	Vector3 vAxisY = Vector3(tempMatrix.m[1][0], tempMatrix.m[1][1], tempMatrix.m[1][2]);
+	Vector3 vAxisZ = Vector3(tempMatrix.m[2][0], tempMatrix.m[2][1], tempMatrix.m[2][2]);
+
+	Vector3 vDelta = WorldLocation - vCamera.Location;
+	Vector3 vTransformed = Vector3(vDelta.Dot(vAxisY), vDelta.Dot(vAxisZ), vDelta.Dot(vAxisX));
+
+	if (vTransformed.z < 1.f)
+		vTransformed.z = 1.f;
+
+	return Vector2((Width / 2.0f) + vTransformed.x * (((Width / 2.0f) / tanf(vCamera.FieldOfView * (float)M_PI / 360.f))) / vTransformed.z, (Height / 2.0f) - vTransformed.y * (((Width / 2.0f) / tanf(vCamera.FieldOfView * (float)M_PI / 360.f))) / vTransformed.z);
+}
